d9aaa79f497b44c8fdf48f5e5de24f4fcade96f3
EDev <eduardolmedi00@gmail.com>
2022-08-07 19:41:02 -0600

    middlewares updated

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,22 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent){
      
-     const { role = '' } = req.cookies;
+     const role = req.cookies.get('role')
 
-     if(role !== 'admin'){
+     if(!role || role !== 'admin'){
           const url = req.nextUrl.clone();
           url.pathname = "/";
           return NextResponse.redirect(url)
      }
 
+    
+
 
      return NextResponse.next()
      
+}
 
+
+export const config = {
+     matcher: ['/actions/add', '/actions/update/:path']
 }
